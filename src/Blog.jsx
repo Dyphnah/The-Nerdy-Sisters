3770f466df2a490909cd1f03a7fe4d83f2ed50b6
Dyphnah <nyandukodyphnah4@gmail.com>
2025-09-15 07:54:24 -0400

    updated blog page

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -1,9 +1,44 @@
-import "./Blog.css"
+import "./Blog.css";
+import { useState, useEffect } from "react";
 
-export default function Blog() {
-    
+function Blog() {
+  const [posts, setPosts] = useState([]);
+  const api = "http://127.0.0.1:8000/api/blogs";
+
+  useEffect(() => {
+    fetch(api)
+        .then((res) => res.json())
+        .then((data) => setPosts(data))
+        .catch(err => console.error("Fetch error:", err));
+  }, []);
+
     return (
+    <>
+    
+    <h1> Welcome to My blog Page</h1>
         
-        <h1 className="centered-line">I’ll make this page live once the backend is fully developed.</h1>
-    )
+    <div className="blog-section">
+      {posts.map((post) => (
+        <div key={post.id} className="blog-card">
+          <p><strong>Last updated:</strong> {post.last_updated}</p>
+
+          {post.image && (
+            <img
+              src={post.image}
+              alt={post.title}
+            />
+          )}
+
+          <h2>{post.title}</h2>
+          <h4>Written By: {post.author}</h4>
+          <p><strong>Category:</strong> {post.category}</p>
+          <p>{post.description}</p>
+          <p><em>Slug:</em> {post.slug}</p>
+        </div>
+      ))}
+    </div>
+    </>
+  );
 }
+
+export default Blog;
--- src/Home.jsx
+++ src/Home.jsx
@@ -46,42 +46,42 @@ export default function Home() {
               <img src={design} alt="Administrative Support" />
               <h3>Admin Support</h3>
               <p>We’re here to make your life easier! With our email management and calendar scheduling, you’ll never miss an important message or appointment again. We’ll keep your files organized so you can quickly find what you need. Plus, we handle travel arrangements and follow up with clients, saving you time and reducing stress. Our project management support and task prioritization mean you can focus on growing your business while we take care of the details that keep everything running smoothly.</p>
-              <p className="price">Starting at $21 per hour</p>
+              {/* <p className="price">Starting at $21 per hour</p> */}
               <a href="consult-page-url" className="cta-button">Book a Consultation</a>
             </div>
             <div className="service-item">
               <img src={design} alt="Content Creation" />
               <h3>Content Creation</h3>
               <p>We’ll bring your ideas to life through high-quality content creation! Whether you need engaging blog posts, informative technical documents, persuasive email copy, or compelling sales and landing pages, we’ve got you covered. Our team understands how to tailor content to resonate with your audience, ensuring it not only informs but also inspires action. Let us help you connect with your audience and elevate your brand through powerful storytelling and effective communication!</p>
-              <p className="price">Starting at $30</p>
+              {/* <p className="price">Starting at $30</p> */}
               <a href="consult-page-url" className="cta-button">Book a Consultation</a>
             </div>
             <div className="service-item">
               <img src={design} alt="Front-end Development" />
               <h3>Front-end Development</h3>
               <p>With our technical expertise, we can create stunning, responsive designs that adapt seamlessly to any device. Using React & CSS, we build interactive user interfaces that enhance the user experience and engage your audience. Our flexible approach allows us to bring your unique vision to life while ensuring optimal performance.</p>
-              <p className="price">Starting at $60</p>
+              {/* <p className="price">Starting at $60</p> */}
               <a href="consult-page-url" className="cta-button">Book a Consultation</a>
             </div>
             <div className="service-item">
               <img src={design} alt="Social Media Management" />
               <h3>Social Media Management</h3>
               <p>We’ll help you connect with your audience in meaningful ways! We'll craft tailored strategies to boost your online presence, increase engagement, and grow your brand. We handle everything from creating eye-catching content and scheduling posts to monitoring analytics and responding to followers.</p>
-              <p className="price">5 Posts per week from $100</p>
+              {/* <p className="price">5 Posts per week from $100</p> */}
               <a href="consult-page-url" className="cta-button">Book a Consultation</a>
             </div>
             <div className="service-item">
               <img src={design} alt="Graphic Design" />
               <h3>Graphic Design</h3>
               <p>We love bringing creativity to life with graphic design! Whether you need stunning social media graphics, sleek presentations, or eye-catching promotional materials, we’ve got you covered. Our team blends creativity with simplicity, ensuring your visuals are not only engaging but also easy to understand.</p>
-              <p className="price">Starting at $20</p>
+              {/* <p className="price">Starting at $20</p> */}
               <a href="consult-page-url" className="cta-button">Book a Consultation</a>
             </div> 
             <div className="service-item">
               <img src={design} alt="Transcription & Video Captioning" />
               <h3>Transcription & Video Captioning</h3>
               <p>We’re here to make transcription and video captioning hassle-free! Our team provides accurate and timely transcriptions of your audio or video files. We also specialize in video captioning, making your content accessible to a wider audience.</p>
-              <p className="price">Starting at $50 per audio hour</p>
+              {/* <p className="price">Starting at $50 per audio hour</p> */}
               <a href="consult-page-url" className="cta-button">Book a Consultation</a>
             </div>          
           </div>
@@ -92,7 +92,7 @@ export default function Home() {
           <div className="grid-container">
             <div className="package">
               <h3>Basic Plan</h3>
-              <p className="rate">$399/month</p>
+              {/* <p className="rate">$399/month</p> */}
               <ul>
                 <li>Up to 20 hours of support per month</li>
                 <li>Switch Plans or Cancel Anytime</li>
@@ -104,7 +104,7 @@ export default function Home() {
 
             <div className="package">
               <h3>Standard Plan</h3>
-              <p className="rate">$750/month</p>
+              {/* <p className="rate">$750/month</p> */}
               <ul>
                 <li>Up to 40 hours of support per month</li>
                 <li>Switch Plans or Cancel Anytime</li>
@@ -116,7 +116,7 @@ export default function Home() {
 
             <div className="package">
               <h3>Premium Plan</h3>
-              <p className="rate">$1799/month</p>
+              {/* <p className="rate">$1799/month</p> */}
               <ul>
                 <li>Up to 100 hours of support per month</li>
                 <li>Switch Plans or Cancel Anytime</li>
